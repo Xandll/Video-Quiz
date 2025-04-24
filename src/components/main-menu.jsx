4b88865f0e1682dcab48d572d4048caa396e53ae
Jaegeti <jakob.m.rehrl@gmail.com>
2025-04-24 10:47:04 +0200

    main-menu.jsx

--- src/components/main-menu.jsx
+++ src/components/main-menu.jsx
@@ -1,6 +1,12 @@
 import './main-menu.css';
 import React, { useState } from 'react';
+import logo from "/public/logo.png"
 
 export default function MainMenu({ onStart }) {
-    return
+    return(
+        <>
+            <header><img src="{logo}" alt="logo"/></header>
+            <h1>VIDEO QUIZ</h1>
+        </>
+    )
 }
